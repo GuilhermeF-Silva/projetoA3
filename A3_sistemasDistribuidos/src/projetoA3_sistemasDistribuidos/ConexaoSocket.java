8a233bf55839f289a7a7e2fc390a92915b497ca6
GuilhermeF-Silva <guilhermef.silva7460@gmail.com>
2024-06-08 21:20:23 -0300

    Tratativa de excessão
    Foi adicionado tratativa de excessão para os casos de:
    ip do servidor errado
    escolhas erradas
    Adicionado as opçoes de jogar novamente e sair

--- A3_sistemasDistribuidos/src/projetoA3_sistemasDistribuidos/ConexaoSocket.java
+++ A3_sistemasDistribuidos/src/projetoA3_sistemasDistribuidos/ConexaoSocket.java
@@ -1,13 +1,14 @@
 package projetoA3_sistemasDistribuidos;
+
 import java.io.*;
 import java.net.*;
 import java.util.Random;
 
-public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão com o cliente, estendendo Thread para permitir execução em paralelo
+public class ConexaoSocket extends Thread {
     private Socket socket;
     private PrintWriter escritor;
     private BufferedReader leitor;
-    private static final Object lock = new Object(); 
+    private static final Object lock = new Object();
     private static ConexaoSocket primeiroCliente = null;
     private static ConexaoSocket segundoCliente = null;
     private static int rodada = 0;
@@ -28,17 +29,25 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
             OutputStream output = socket.getOutputStream();
             escritor = new PrintWriter(new OutputStreamWriter(output, "UTF-8"), true);
 
-            escritor.println("Bem vindo ao jokenpo, por favor escolha se deseja jogar contra um robo ou contra outro jogador \n 1 - Robo (CPU) \n 2 - Outro Jogador");
+            escritor.println("Bem vindo ao jokenpo, por favor escolha se deseja jogar contra um robo ou contra outro jogador \n 1 - Robo (CPU) \n 2 - Outro Jogador. \n Faça sua escolha:");
 
             String escolhaModo = leitor.readLine();
             System.out.println("Escolha do cliente: " + escolhaModo); // Depuração
 
-            if ("1".equals(escolhaModo)) {
-                jogarContraRobo();
-            } else if ("2".equals(escolhaModo)) {
-                jogarContraOutroJogador();
-            } else {
-                escritor.println("Escolha inválida. Encerrando conexão.");
+            // Modificação para tratar escolhas inválidas
+            while (!escolhaModo.equals("1") && !escolhaModo.equals("2")) {
+                escritor.println("Escolha inválida. Escolha outra opção \n 1 - Robo (CPU) \n 2 - Outro Jogador. \n Faça sua escolha:");
+                escolhaModo = leitor.readLine();
+                System.out.println("Escolha do cliente: " + escolhaModo); // Depuração
+            }
+
+            switch (escolhaModo) {
+                case "1":
+                    jogarContraRobo();
+                    break;
+                case "2":
+                    jogarContraOutroJogador();
+                    break;
             }
 
         } catch (IOException | InterruptedException ex) {
@@ -59,20 +68,33 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
         int totalRodadas = 3;
         int[] estatisticas = {0, 0, 0}; // Vitórias, Derrotas, Empates
 
-        escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura");
+        escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
 
         while (rodada < totalRodadas) {
             String escolhaJogador = leitor.readLine();
+            System.out.println("Escolha do jogador: " + escolhaJogador); // Depuração
+
+            // Tratar escolhas inválidas
+            while (escolhaJogador == null ||
+                   escolhaJogador.isEmpty() ||
+                   (!escolhaJogador.equalsIgnoreCase("pedra") &&
+                    !escolhaJogador.equalsIgnoreCase("papel") &&
+                    !escolhaJogador.equalsIgnoreCase("tesoura"))) {
+                escritor.println("Escolha inválida. Escolha novamente entre pedra, papel ou tesoura. \n Faça sua escolha:");
+                escolhaJogador = leitor.readLine();
+                System.out.println("Nova escolha do jogador: " + escolhaJogador); // Depuração
+            }
+
             String escolhaRobo = opcoes[random.nextInt(opcoes.length)];
 
             escritor.println("Robô escolheu: " + escolhaRobo);
 
-            if (escolhaJogador.equals(escolhaRobo)) {
+            if (escolhaJogador.equalsIgnoreCase(escolhaRobo)) {
                 escritor.println("Empate!");
                 estatisticas[2]++;
-            } else if ((escolhaJogador.equals("pedra") && escolhaRobo.equals("tesoura")) ||
-                       (escolhaJogador.equals("papel") && escolhaRobo.equals("pedra")) ||
-                       (escolhaJogador.equals("tesoura") && escolhaRobo.equals("papel"))) {
+            } else if ((escolhaJogador.equalsIgnoreCase("pedra") && escolhaRobo.equals("tesoura")) ||
+                       (escolhaJogador.equalsIgnoreCase("papel") && escolhaRobo.equals("pedra")) ||
+                       (escolhaJogador.equalsIgnoreCase("tesoura") && escolhaRobo.equals("papel"))) {
                 escritor.println("Você ganhou a rodada!");
                 estatisticas[0]++;
             } else {
@@ -83,11 +105,26 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
             escritor.println("Estatísticas da rodada - Vitórias: " + estatisticas[0] + ", Derrotas: " + estatisticas[1] + ", Empates: " + estatisticas[2]);
             rodada++;
             if (rodada < totalRodadas) {
-                escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura");
+                escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
             }
         }
 
         escritor.println("Estatísticas da partida - Vitórias: " + estatisticas[0] + ", Derrotas: " + estatisticas[1] + ", Empates: " + estatisticas[2]);
+
+        // Perguntar se o jogador deseja jogar novamente ou sair
+        while (true) {
+            escritor.println("Deseja jogar novamente ou sair? \n Digite 'novamente' para jogar novamente ou 'sair' para encerrar.\n Faça sua escolha:");
+            String escolhaFinal = leitor.readLine();
+            if (escolhaFinal.equalsIgnoreCase("novamente")) {
+                run();
+                break;
+            } else if (escolhaFinal.equalsIgnoreCase("sair")) {
+                escritor.println("Obrigado por jogar! Até a próxima.");
+                break;
+            } else {
+                escritor.println("Escolha inválida, tente novamente.");
+            }
+        }
     }
 
     private void jogarContraOutroJogador() throws IOException, InterruptedException {
@@ -97,7 +134,7 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
                 escritor.println("Esperando outro jogador se conectar...");
                 while (segundoCliente == null) {
                     try {
-                        lock.wait(); 
+                        lock.wait();
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
@@ -105,23 +142,26 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
                 iniciarJogo();
             } else {
                 segundoCliente = this;
-                lock.notifyAll(); 
+                lock.notifyAll();
             }
         }
 
         RespostaJogadores();
-        
+
         if (rodada == totalRodadas) {
             finalizarJogo();
         }
+       
     }
 
     private void iniciarJogo() {
-        primeiroCliente.escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura");
-        segundoCliente.escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura");
+        rodada = 0;
+        escolhas = new String[2];
+        primeiroCliente.escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
+        segundoCliente.escritor.println("O jogo começou. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
     }
 
-    private void determinarVencedorRodada() throws InterruptedException, IOException {
+    private void determinarVencedorRodada() {
         rodada++;
         String resultado;
         if (escolhas[0].equals(escolhas[1])) {
@@ -144,19 +184,28 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
         segundoCliente.escritor.println("Estatísticas da rodada - Vitórias: " + segundoCliente.estatisticas[0] + ", Derrotas: " + segundoCliente.estatisticas[1] + ", Empates: " + segundoCliente.estatisticas[2]);
 
         if (rodada < totalRodadas) {
-            primeiroCliente.escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura");
-            segundoCliente.escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura");
+            primeiroCliente.escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
+            segundoCliente.escritor.println("Próxima rodada. Escolha entre pedra, papel ou tesoura. \n Faça sua escolha:");
         }
     }
 
     private void finalizarJogo() {
         primeiroCliente.escritor.println("Estatísticas da partida - Vitórias: " + primeiroCliente.estatisticas[0] + ", Derrotas: " + primeiroCliente.estatisticas[1] + ", Empates: " + primeiroCliente.estatisticas[2]);
+        primeiroCliente.escritor.println("O Jogo está sendo finalizado, obrigado por jogar com nós");
         segundoCliente.escritor.println("Estatísticas da partida - Vitórias: " + segundoCliente.estatisticas[0] + ", Derrotas: " + segundoCliente.estatisticas[1] + ", Empates: " + segundoCliente.estatisticas[2]);
+         segundoCliente.escritor.println("O Jogo está sendo finalizado, obrigado por jogar com nós");
     }
 
     private void RespostaJogadores() throws InterruptedException, IOException {
         while (rodada < totalRodadas) {
-            String escolha = leitor.readLine(); 
+            String escolha = leitor.readLine();
+            while (!escolha.equalsIgnoreCase("pedra") &&
+                   !escolha.equalsIgnoreCase("papel") &&
+                   !escolha.equalsIgnoreCase("tesoura")) {
+                escritor.println("Escolha inválida. Escolha novamente entre pedra, papel ou tesoura. \n Faça sua escolha:");
+                escolha = leitor.readLine();
+            }
+
             synchronized (lock) {
                 if (this == primeiroCliente) {
                     escolhas[0] = escolha;
@@ -165,7 +214,7 @@ public class ConexaoSocket extends Thread { // Classe para gerenciar a conexão 
                     escolhas[1] = escolha;
                     escritor.println("Esperando resposta do outro jogador...");
                 }
-                
+
                 if (escolhas[0] != null && escolhas[1] != null) {
                     determinarVencedorRodada();
                     escolhas[0] = null;
